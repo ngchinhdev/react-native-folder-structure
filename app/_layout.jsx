5baa4e2cadc6cb3241422e00c4302f3a81219e6f
sw-n2-9h <chinhnguyennn24@gmail.com>
2024-08-21 14:19:24 +0700

    initial commit

---
+++ app/_layout.jsx
@@ -0,0 +1,11 @@
+import { Stack } from 'expo-router';
+
+export default function RootLayout() {
+    return (
+        <Stack>
+            <Stack.Screen name='index' options={ {
+                title: "Home pageeees"
+            } } />
+        </Stack>
+    );
+}
---
+++ app/index.jsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+const Home = () => {
+    return (
+        <View className="text-red-600 pt-32 px-10">
+            <Text className="text-teal-400 text-center">Open up App.js to start working on your app! My first App.</Text>
+        </View>
+    );
+};
+
+export default Home;
